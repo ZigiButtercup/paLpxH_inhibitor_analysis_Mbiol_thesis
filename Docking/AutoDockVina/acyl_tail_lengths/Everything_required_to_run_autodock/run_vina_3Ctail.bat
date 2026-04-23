124c6dd3df1f2c48f5b61e5245f02ef4c57ca83f
HB <YOUR_EMAIL>
2026-04-23 15:22:46 +0100

    initial clean commit: paLpxH inhibitor project

---
+++ Docking/AutoDockVina/acyl_tail_lengths/Everything_required_to_run_autodock/run_vina_3Ctail.bat
@@ -0,0 +1,29 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: === USER SETTINGS ===
+set "receptor=paLpxH.pdbqt"    :: change to your receptor file
+set "ligand=3Ctail.pdbqt"       :: change to your ligand file
+set runs=10                      :: how many repeats
+
+for %%A in ("%receptor%") do set "receptorName=%%~nA"
+for %%B in ("%ligand%")  do set "ligandName=%%~nB"
+
+for /L %%i in (1,1,%runs%) do (
+    echo Running docking iteration %%i...
+    vina_1.2.7_win.exe ^
+      --receptor "!receptor!" ^
+      --ligand  "!ligand!"  ^
+      --center_x 10.889      ^
+      --center_y 1.139      ^
+      --center_z -6.667      ^
+      --size_x   72          ^
+      --size_y   84          ^
+      --size_z   76          ^
+      --exhaustiveness 40     ^
+      --seed %%i             ^
+      --out "!receptorName!_!ligandName!_%%i.pdbqt"
+)
+
+echo All runs completed.
+pause
